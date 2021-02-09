7527a9b7dd59871fc231d613ae4297434771e092
Aditya <aditya.cognisense13@gmail.com>
2021-02-09 11:23:26 +0530

    Done with the insertion data

--- CMS_TEMPLATE/index.php
+++ CMS_TEMPLATE/index.php
@@ -15,6 +15,7 @@ include "includes/navigation.php";
 
             $query="SELECT * FROM posts";
             $select_all_posts_query=mysqli_query($connection,$query);
+             
             while($row = mysqli_fetch_assoc($select_all_posts_query))
             {
              $post_title=   $row['post_title'];
@@ -22,15 +23,9 @@ include "includes/navigation.php";
              $post_date=$row['post_date'];
              $post_image=$row['post_image'];
              $post_content=$row['post_content'];
-            
-            }
-            ?>
+             ?> 
 
-
-
-
-
-                <h1 class="page-header">
+<h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
@@ -41,58 +36,36 @@ include "includes/navigation.php";
                     echo $post_title ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                    by <a href="index.php"><?php
+                    echo $post_author ?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on February 09, 2021 at 10:51 AM</p>
+                <p><span class="glyphicon glyphicon-time"></span><?php
+                    echo $post_date ?></p>
                 <hr>
                 <img class="img-responsive" src="http://placehold.it/900x300" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                <p><?php
+                    echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
 
-                <!-- Second Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit voluptates!</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                <hr>
 
-                <!-- Third Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, voluptates, voluptas dolore ipsam cumque quam veniam accusantium laudantium adipisci architecto itaque dicta aperiam maiores provident id incidunt autem. Magni, ratione.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                <hr>
+<?php           
+  }
+            
+            ?>
+               
+
+               
+            
 
                 <!-- Pager -->
-                <ul class="pager">
-                    <li class="previous">
-                        <a href="#">&larr; Older</a>
-                    </li>
-                    <li class="next">
-                        <a href="#">Newer &rarr;</a>
-                    </li>
-                </ul>
+                <?php
+                include "includes/pager.php";
+                ?>
 
             </div>
 
