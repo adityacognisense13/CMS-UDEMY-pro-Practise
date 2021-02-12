5f3b9064b8419d74b59569c90dedef1b8243b3d2
Aditya <aditya.cognisense13@gmail.com>
2021-02-12 11:49:40 +0530

    minute changes in navigation

--- CMS_TEMPLATE/admin/includes/navigation.php
+++ CMS_TEMPLATE/admin/includes/navigation.php
@@ -39,7 +39,8 @@
                         <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i>Posts <i class="fa fa-fw fa-caret-down"></i></a>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i>Posts 
+                        <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="posts_dropdown" class="collapse">
                             <li>
                                 <a href="#">Add Posts</a>
